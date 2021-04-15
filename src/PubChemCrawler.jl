ffabaf041df9985166716de7d8de4068543e97c2
Hazel Brown <hazelbrown@Hazels-MacBook-Air.local>
2021-04-15 19:45:55 -0400

    export query_superstructure, query_similarity_2d, query_similarity_3d

--- src/PubChemCrawler.jl
+++ src/PubChemCrawler.jl
@@ -3,7 +3,7 @@ module PubChemCrawler
 using HTTP
 using LightXML
 
-export atomregex, parse_formula, get_cid, get_for_cids, query_substructure, query_substructure_pug
+export atomregex, parse_formula, get_cid, get_for_cids, query_substructure, query_superstructure, query_similarity_2d, query_similarity_3d, query_substructure_pug
 
 include("utils.jl")
 include("query.jl")
